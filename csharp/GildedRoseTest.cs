3d94b49620bb159f041639480de5fa7e84705702
Tim Hurrell <tim@hurrell.org>
2024-04-11 20:55:58 +0100

    Added a brie item test

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -16,5 +16,16 @@ namespace csharp
             //
             //
         }
+
+        [Test]
+        public void brie()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(-1, Items[0].SellIn);
+            Assert.AreEqual(1, Items[0].Quality);
+        }
+
     }
 }
