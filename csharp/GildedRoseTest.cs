e2fec408ec0b8e829967020e5f97ea2930d1fb17
Tim Hurrell <tim@hurrell.org>
2024-04-12 21:21:16 +0100

    Added Sulfuras test

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,7 +7,7 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        /*public void foo()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
             GildedRose app = new GildedRose(Items);
@@ -18,8 +18,9 @@ namespace csharp
             //
             //
         }
+        */
 
-        [Test]
+       
         public void brie()
         {
             IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 0 } };
@@ -27,6 +28,16 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual(-1, Items[0].SellIn);
             Assert.AreEqual(1, Items[0].Quality);
+        }
+
+        [Test]
+        public void sulfuras()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].SellIn);
+            Assert.AreEqual(0, Items[0].Quality);
         }
 
     }
