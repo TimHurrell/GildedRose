1f9726bda05fc2d80df417faaf7ba603e769c805
Tim Hurrell <tim@hurrell.org>
2024-04-13 15:00:27 +0100

    Created a full set of tests for brie and sulfuras (at least for the time being)

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -27,7 +27,37 @@ namespace csharp
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.AreEqual(-1, Items[0].SellIn);
-            Assert.AreEqual(1, Items[0].Quality);
+            Assert.AreEqual(2, Items[0].Quality);
+
+            IList<Item> Items1 = new List<Item> { new Item { Name = "Aged Brie", SellIn = 30, Quality = 30 } };
+            app = new GildedRose(Items1);
+            app.UpdateQuality();
+            Assert.AreEqual(29, Items1[0].SellIn);
+            Assert.AreEqual(31, Items1[0].Quality);
+
+            IList<Item> Items2 = new List<Item> { new Item { Name = "Aged Brie", SellIn = 7, Quality = 7 } };
+            app = new GildedRose(Items2);
+            app.UpdateQuality();
+            Assert.AreEqual(6, Items2[0].SellIn);
+            Assert.AreEqual(8, Items2[0].Quality);
+
+            IList<Item> Items3 = new List<Item> { new Item { Name = "Aged Brie", SellIn = 1, Quality = 1 } };
+            app = new GildedRose(Items3);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items3[0].SellIn);
+            Assert.AreEqual(2, Items3[0].Quality);
+
+            IList<Item> Items4 = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 1 } };
+            app = new GildedRose(Items4);
+            app.UpdateQuality();
+            Assert.AreEqual(-1, Items4[0].SellIn);
+            Assert.AreEqual(3, Items4[0].Quality);
+
+            IList<Item> Items5 = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 50 } };
+            app = new GildedRose(Items5);
+            app.UpdateQuality();
+            Assert.AreEqual(-1, Items5[0].SellIn);
+            Assert.AreEqual(50, Items5[0].Quality);
         }
 
         [Test]
@@ -38,6 +68,30 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual(0, Items[0].SellIn);
             Assert.AreEqual(0, Items[0].Quality);
+
+            IList<Item> Items1 = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 30, Quality = 30 } };
+            app = new GildedRose(Items1);
+            app.UpdateQuality();
+            Assert.AreEqual(30, Items1[0].SellIn);
+            Assert.AreEqual(30, Items1[0].Quality);
+
+            IList<Item> Items2 = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 7, Quality = 7 } };
+            app = new GildedRose(Items2);
+            app.UpdateQuality();
+            Assert.AreEqual(7, Items2[0].SellIn);
+            Assert.AreEqual(7, Items2[0].Quality);
+
+            IList<Item> Items4 = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 1 } };
+            app = new GildedRose(Items4);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items4[0].SellIn);
+            Assert.AreEqual(1, Items4[0].Quality);
+
+            IList<Item> Items5 = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 50 } };
+            app = new GildedRose(Items5);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items5[0].SellIn);
+            Assert.AreEqual(50, Items5[0].Quality);
         }
 
     }
