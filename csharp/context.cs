f2a4230e98536f568e1fda1283eb72a6c87e4881
Tim Hurrell <tim@hurrell.org>
2024-04-25 18:55:22 +0100

    Added context. Not tested this nor the interface yet. Other tests pass aprat from UAT.

---
+++ csharp/context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace csharp
+{
+    public class Context
+    {
+        private IGildedRoseStrategy? _GildedRoseStrategy;
+
+        public void SetStrategy(IGildedRoseStrategy strategy)
+        {
+            _GildedRoseStrategy = strategy;
+        }
+
+    }
+}
